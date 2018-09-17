8a890f0f30d093e929f30cf533220d1a0d2c1b81
mqadri93 <mohamadqadri93@gmail.com>
2018-09-16 21:51:26 -0400

    Update word-ladder-ii.py

--- word-ladder-ii.py
+++ word-ladder-ii.py
@@ -1,4 +1,5 @@
 
+
 class Solution:
     def findLadders(self, beginWord, endWord, wordList):
         """
@@ -10,7 +11,7 @@ class Solution:
         if not endWord in wordList:
             return []
         self.Graph = {}
-        self.BFS(beginWord, wordList)
+        self.BFS(beginWord, endWord, wordList)
         self.optimal = self.level.get(endWord)
         if not self.optimal:
             return []
@@ -31,20 +32,25 @@ class Solution:
         for s in wordList:
             if u!=s and self.differ_by_one(u, s):
                 neighbours.append(s)
-        return neighbours
+        return neighbours  
     
-    def BFS(self, beginWord, wordList):
+    def BFS(self, beginWord, endWord, wordList):
         level = {beginWord: 0}
         i = 1 
         frontier = [beginWord]
+        found = False
         while frontier:
             next1 = []
             for u in frontier:
                 self.Graph[u] = self.find_neighbours(u, wordList)
                 for v in self.Graph[u]:
+                    if endWord == v:
+                        found = True
                     if v not in level:
                         level[v] = i
                         next1.append(v)
+            if found:
+                break
             frontier = next1
             i +=1
         self.level = level
@@ -54,9 +60,10 @@ class Solution:
         if end==start and len(solution) ==  self.optimal + 1:
             res.append([c for c in solution])
         else:
-            for next in self.Graph[start]:
-                if self.level and self.level[next]==self.level[start]+1:
-                    self.DFS(next, end, solution, res)
+            if len(solution) !=  self.optimal + 1:
+                for next in self.Graph[start]:
+                    if self.level[next]==self.level[start]+1:
+                        self.DFS(next, end, solution, res)
         del solution[len(solution)-1]
         
             
