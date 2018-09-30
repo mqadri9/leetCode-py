e0e37820da20b2b7dc9e5366cfccbb93fa020dae
mqadri93 <mohamadqadri93@gmail.com>
2018-09-30 15:38:53 -0400

    Update word-break.py

--- word-break.py
+++ word-break.py
@@ -22,3 +22,31 @@ class Solution(object):
             return res
         return traverse(0, s, wordDict, d)
             
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        def traverse(s, wordDict, d):
+            if s in d:
+                return d[s]
+            for n in range(1, len(s)+1):
+                try:
+                    considered = s[:n]
+                    not_considered = s[n:]
+                except:
+                    considered = s
+                    not_considered = ""
+                if considered in wordDict:
+                    if not_considered == "":
+                        return True
+                    res = traverse(not_considered, wordDict, d)
+                    if res:
+                        d[s] = True
+                        return True
+            d[s] = False
+            return False
+            
+        d = {}
+        return traverse(s, wordDict, d)
